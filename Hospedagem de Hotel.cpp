248c42bda745b57903da3ace7e2aec711f8e4787
RamonSouzaDev <michael@example.com>
2019-02-08 16:06:14 -0200

    Calculo de estadia do hospede

---
+++ Hospedagem de Hotel.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL,"");
+	
+	int dias;
+	float totalTaxa, totalEstadia, total;
+	
+	
+	
+	printf("Digite a quantidade de dias que você deseja permanecer no hotel:");
+	scanf("%d",&dias);
+	
+	if(dias>15){
+		totalTaxa = (dias * 5.50);
+		totalEstadia = (dias * 60.00);
+		total = totalTaxa + totalEstadia;
+		
+		}else if(dias==15){
+			totalTaxa = (dias * 6.00);
+			totalEstadia = (dias * 60.00);
+			total = totalTaxa + totalEstadia;
+			
+				}else if(dias<15){
+					totalTaxa = (dias * 8.00);
+					totalEstadia = (dias * 60.00);
+					total = totalTaxa + totalEstadia;
+			    	}
+		
+		
+		
+		printf("Valor da estadia:.....%.2f\n O valor dos serviços:.....%.2f\n Valor Total A pagar.....%.2f",totalTaxa,totalEstadia,total);
+	}
+	
