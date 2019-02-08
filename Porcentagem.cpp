56c3eeec43ddad2960b62e98565ab66fea94566c
RamonSouzaDev <dwmom@hotmail.com>
2019-02-08 14:42:41 -0200

    Inserindo Projeto

---
+++ Porcentagem.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL,"");
+	
+	float porcem, total, rest;
+	
+	printf("Digite a porcetagem:");
+	scanf("%f",&porcem);
+	
+	system("cls");
+	
+	printf("\n %.0f porcento de quanto quer calcular:", porcem);
+	scanf("%f",&total);
+	
+	rest = (porcem*total)/100;
+	printf("\nResultado é : %.0f", rest);
+	
+	
+}
